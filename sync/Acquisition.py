479def501df35309aaaed6d00088df171aafd306
fuhua963 <2227171051@qq.com>
2024-11-30 18:56:19 +0800

    唉唉

---
+++ sync/Acquisition.py
@@ -0,0 +1,266 @@
+import os
+import PySpin
+import sys
+
+NUM_IMAGES = 10  # number of images to grab
+
+
+def acquire_images(cam, nodemap, nodemap_tldevice):
+
+
+    print('*** IMAGE ACQUISITION ***\n')
+    try:
+        result = True
+
+        # In order to access the node entries, they have to be casted to a pointer type (CEnumerationPtr here)
+        node_acquisition_mode = PySpin.CEnumerationPtr(nodemap.GetNode('AcquisitionMode'))
+        if not PySpin.IsReadable(node_acquisition_mode) or not PySpin.IsWritable(node_acquisition_mode):
+            print('Unable to set acquisition mode to continuous (enum retrieval). Aborting...')
+            return False
+
+        # Retrieve entry node from enumeration node
+        node_acquisition_mode_continuous = node_acquisition_mode.GetEntryByName('Continuous')
+        if not PySpin.IsReadable(node_acquisition_mode_continuous):
+            print('Unable to set acquisition mode to continuous (entry retrieval). Aborting...')
+            return False
+        # Retrieve integer value from entry node
+        acquisition_mode_continuous = node_acquisition_mode_continuous.GetValue()
+        # Set integer value from entry node as new value of enumeration node
+        node_acquisition_mode.SetIntValue(acquisition_mode_continuous)
+
+        print('Acquisition mode set to continuous...')
+        cam.BeginAcquisition()
+
+        print('Acquiring images...')
+
+        #  Retrieve device serial number for filename
+        #
+        #  *** NOTES ***
+        #  The device serial number is retrieved in order to keep cameras from
+        #  overwriting one another. Grabbing image IDs could also accomplish
+        #  this.
+        device_serial_number = ''
+        node_device_serial_number = PySpin.CStringPtr(nodemap_tldevice.GetNode('DeviceSerialNumber'))
+        if PySpin.IsReadable(node_device_serial_number):
+            device_serial_number = node_device_serial_number.GetValue()
+            print('Device serial number retrieved as %s...' % device_serial_number)
+
+        # Retrieve, convert, and save images
+
+        # Create ImageProcessor instance for post processing images
+        processor = PySpin.ImageProcessor()
+
+        # Set default image processor color processing method
+        #
+        # *** NOTES ***
+        # By default, if no specific color processing algorithm is set, the image
+        # processor will default to NEAREST_NEIGHBOR method.
+        processor.SetColorProcessing(PySpin.SPINNAKER_COLOR_PROCESSING_ALGORITHM_HQ_LINEAR)
+
+        for i in range(NUM_IMAGES):
+            try:
+
+                #  Retrieve next received image
+                #
+                #  *** NOTES ***
+                #  Capturing an image houses images on the camera buffer. Trying
+                #  to capture an image that does not exist will hang the camera.
+                #
+                #  *** LATER ***
+                #  Once an image from the buffer is saved and/or no longer
+                #  needed, the image must be released in order to keep the
+                #  buffer from filling up.
+                image_result = cam.GetNextImage(1000)
+
+                #  Ensure image completion
+                #
+                #  *** NOTES ***
+                #  Images can easily be checked for completion. This should be
+                #  done whenever a complete image is expected or required.
+                #  Further, check image status for a little more insight into
+                #  why an image is incomplete.
+                if image_result.IsIncomplete():
+                    print('Image incomplete with image status %d ...' % image_result.GetImageStatus())
+
+                else:
+
+                    #  Print image information; height and width recorded in pixels
+                    #
+                    #  *** NOTES ***
+                    #  Images have quite a bit of available metadata including
+                    #  things such as CRC, image status, and offset values, to
+                    #  name a few.
+                    width = image_result.GetWidth()
+                    height = image_result.GetHeight()
+                    print('Grabbed Image %d, width = %d, height = %d' % (i, width, height))
+
+                    #  Convert image to mono 8
+                    #
+                    #  *** NOTES ***
+                    #  Images can be converted between pixel formats by using
+                    #  the appropriate enumeration value. Unlike the original
+                    #  image, the converted one does not need to be released as
+                    #  it does not affect the camera buffer.
+                    #
+                    #  When converting images, color processing algorithm is an
+                    #  optional parameter.
+                    image_converted = processor.Convert(image_result, PySpin.PixelFormat_Mono8)
+
+                    # Create a unique filename
+                    if device_serial_number:
+                        filename = 'Acquisition-%s-%d.jpg' % (device_serial_number, i)
+                    else:  # if serial number is empty
+                        filename = 'Acquisition-%d.jpg' % i
+
+                    #  Save image
+                    #
+                    #  *** NOTES ***
+                    #  The standard practice of the examples is to use device
+                    #  serial numbers to keep images of one device from
+                    #  overwriting those of another.
+                    image_converted.Save(filename)
+                    print('Image saved at %s' % filename)
+
+                    #  Release image
+                    #
+                    #  *** NOTES ***
+                    #  Images retrieved directly from the camera (i.e. non-converted
+                    #  images) need to be released in order to keep from filling the
+                    #  buffer.
+                    image_result.Release()
+                    print('')
+
+            except PySpin.SpinnakerException as ex:
+                print('Error: %s' % ex)
+                return False
+
+        #  End acquisition
+        #
+        #  *** NOTES ***
+        #  Ending acquisition appropriately helps ensure that devices clean up
+        #  properly and do not need to be power-cycled to maintain integrity.
+        cam.EndAcquisition()
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        return False
+
+    return result
+
+
+def print_device_info(nodemap):
+    """
+    This function prints the device information of the camera from the transport
+    layer; please see NodeMapInfo example for more in-depth comments on printing
+    device information from the nodemap.
+
+    :param nodemap: Transport layer device nodemap.
+    :type nodemap: INodeMap
+    :returns: True if successful, False otherwise.
+    :rtype: bool
+    """
+
+    print('*** DEVICE INFORMATION ***\n')
+
+    try:
+        result = True
+        node_device_information = PySpin.CCategoryPtr(nodemap.GetNode('DeviceInformation'))
+
+        if PySpin.IsReadable(node_device_information):
+            features = node_device_information.GetFeatures()
+            for feature in features:
+                node_feature = PySpin.CValuePtr(feature)
+                print('%s: %s' % (node_feature.GetName(),
+                                  node_feature.ToString() if PySpin.IsReadable(node_feature) else 'Node not readable'))
+
+        else:
+            print('Device control information not readable.')
+
+    except PySpin.SpinnakerException as ex:
+        print('Error: %s' % ex)
+        return False
+
+    return result
+
+
+    return result
+
+
+def main():
+
+
+
+    result = True
+
+    # Retrieve singleton reference to system object
+    system = PySpin.System.GetInstance()
+
+    # Get current library version
+    version = system.GetLibraryVersion()
+    print('Library version: %d.%d.%d.%d' % (version.major, version.minor, version.type, version.build))
+
+    # Retrieve list of cameras from the system
+    cam_list = system.GetCameras()
+
+    num_cameras = cam_list.GetSize()
+
+    print('Number of cameras detected: %d' % num_cameras)
+
+    # Finish if there are no cameras
+    if num_cameras == 0:
+
+        # Clear camera list before releasing system
+        cam_list.Clear()
+
+        # Release system instance
+        system.ReleaseInstance()
+
+        print('Not enough cameras!')
+        return False
+
+    # Run example on each camera
+    for i, cam in enumerate(cam_list):
+
+        print('Running example for camera %d...' % i)
+
+        try:
+            result = True
+
+            # Retrieve TL device nodemap and print device information
+            nodemap_tldevice = cam.GetTLDeviceNodeMap()
+
+            result &= print_device_info(nodemap_tldevice)
+
+            # Initialize camera
+            cam.Init()
+
+            # Retrieve GenICam nodemap
+            nodemap = cam.GetNodeMap()
+
+            # Acquire images
+            result &= acquire_images(cam, nodemap, nodemap_tldevice)
+
+            # Deinitialize camera
+            cam.DeInit()
+
+        except PySpin.SpinnakerException as ex:
+            print('Error: %s' % ex)
+            result = False
+            print('Camera %d example complete... \n' % i)
+
+    del cam
+
+    # Clear camera list before releasing system
+    cam_list.Clear()
+
+    # Release system instance
+    system.ReleaseInstance()
+
+    # input('Done! Press Enter to exit...')
+    return result
+
+if __name__ == '__main__':
+    if main():
+        sys.exit(0)
+    else:
+        sys.exit(1)
--- sync/lib/readme.txt
+++
@@ -1,3 +0,0 @@
-注意：根据实际相机分辨率，修改camera_inf.py中的宽高配置
-WIDTH = 640
-HEIGHT = 480
